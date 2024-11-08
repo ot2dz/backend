c18ba8d40de38e5553981b039ac1dde7bf319a25
ot2dz <131069984+ot2dz@users.noreply.github.com>
2024-11-08 17:21:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_URI: 'mongodb+srv://taxi2bot:a19901990@taxibot.xl92t.mongodb.net/?retryWrites=true&w=majority&appName=TaxiBot',
   JWT_SECRET: process.env.JWT_SECRET,
   PORT: process.env.PORT || 4000
 };
